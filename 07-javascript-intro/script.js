e628d9516daf21cfd39228b445451a946c7b888e
leokim0711092 <leokim10073@gmail.com>
2024-01-10 07:17:03 +0000

    Unit 7: 7.2 JavaScript DOM ex 7.2

--- 07-javascript-intro/script.js
+++ 07-javascript-intro/script.js
@@ -1,9 +1,16 @@
 document.onreadystatechange = function (event) {
     if (document.readyState === 'complete') {
-        console.log('Hello from JavaScript5555')
-        const div1 = document.querySelector('div')
-        div1.innerHTML = 'Inside div'
+        console.log('Hello from JavaScript')
         
+        const div1 = document.getElementById('div1')
+        div1.innerHTML = 'Some text for div1'
+        
+        const div2 = document.getElementsByClassName('div-class')[1]
+        div2.innerHTML = 'Some text for div2'
+        
+        const divs = document.getElementsByTagName('div')
+        divs[0].style = "text-decoration:underline;"
+        divs[1].style = "text-decoration:underline;"
     }
 }
 
